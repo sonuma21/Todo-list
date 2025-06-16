c9db84343073173e07bef1affee095ee5b695eb3
Sonu Ma <sonumalmb@gmail.com>
2025-06-16 15:09:07 +0545

    user authenthication

--- resources/views/components/frontend-navbar.blade.php
+++ resources/views/components/frontend-navbar.blade.php
@@ -4,8 +4,27 @@
             Home
         </div>
         <div>
+            @if (!Auth::user())
+                <div class="text-white">
+                    <a href="{{ route('login') }}">
+                        <button class="rounded-xl bg-[var(--green)] px-4 py-2">Login</button>
+                    </a>
+                    <a href="{{ route('register') }}">
+                        <button class="rounded-xl bg-red-600 px-4 py-2">Register</button>
+                    </a>
+                </div>
+            @else
+                <form action="{{route('logout')}}" method="POST">
+                    @csrf
+                    <button  class="rounded-xl bg-red-600 px-4 py-2">Logout
+
+                    </button>
+                </form>
+            @endif
+        </div>
+        <div>
             <button class="px-4 py-2 rounded-2xl font-bold text-white bg-[var(--green)] ">
-                <a href="{{route('task.add')}}">Add Task</a>
+                <a href="{{ route('task.add') }}">Add Task</a>
             </button>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskManager;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskManager::class, 'listTasks'])->name('home');
-Route::get('/addtask', [TaskManager::class, 'addTask'])->name('task.add');
-Route::post('/add/task/post', [TaskManager::class, 'addTaskPost'])->name('task.add.post');
-Route::get('/update/task/{id}', [TaskManager::class, 'updateTaskStatus'])->name('task.update.status');
-Route::get('/delete/task/{id}', [TaskManager::class, 'deleteTaskStatus'])->name('task.delete.status');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware(['auth', 'verified'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/addtask', [TaskManager::class, 'addTask'])->name('task.add');
+    Route::post('/add/task/post', [TaskManager::class, 'addTaskPost'])->name('task.add.post');
+    Route::get('/update/task/{id}', [TaskManager::class, 'updateTaskStatus'])->name('task.update.status');
+    Route::get('/delete/task/{id}', [TaskManager::class, 'deleteTaskStatus'])->name('task.delete.status');
+});
+
+require __DIR__ . '/auth.php';
